812e7d1510120ad687d69612fe640da4f4016cc6
제유빈 <yubinje630@gmail.com>
2021-12-01 20:47:11 +0900

    Java-Beginner MVC & teplate engine | 21-12-01

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,4 +13,13 @@ public class HelloController {
 
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc (@RequestParam("name", required=false) String name, Model model) {
+        model.addAttribute("name", name);
+
+        return "hello-template";
+    }
+
+    
 }
