e954b4e50c8e215f435676a7ec7ff8a2cc8f2ea3
제유빈 <yubinje630@gmail.com>
2021-12-22 22:57:22 +0900

    Java-Beginner Practice - Spring DB Access - Spring Integration Test | 21-12-22

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -1,35 +1,26 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import hello.hellospring.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
-// 단위테스트 : 테스트 성능이 spring container로 하는 것보다 좋음
-class MemberServiceTest {
+// 통합테스트 
+@SpringBootTest  // Spring Container와 테스트를 함께 실행한다.
+@Transactional // 테스트 케이스에 annotation이 있으면 테스트 시작 전에 트랜잭션을 시작하고, 완료 후 항상 롤백한다.
+               // DB상에 데이터가 남지 않아 다음 테스트나 운영 DB에 영향을 주지 않는다.
+class MemberServiceIntegrationTest {
 
 
-    MemberService memberService;
-    MemoryMemberRepository memberRepository;
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
 
-
-
-    @BeforeEach
-    public void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-
-    }
-
-    @AfterEach
-    public void afterEach() {
-        // memberRepository.clearStore();
-    }
 
     @Test
     void join() {
@@ -51,10 +42,10 @@ class MemberServiceTest {
     public void duplicate_join_exception() {
         // given
         Member member1 = new Member();
-        member1.setName("spring");
+        member1.setName("hello");
 
         Member member2 = new Member();
-        member2.setName("spring");
+        member2.setName("hello");
 
         // when
         memberService.join(member1);
