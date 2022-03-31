2f6c9fd7cd5ba134e427d7c904571bfc65175b8c
제유빈 <yubinje630@gmail.com>
2022-03-31 23:13:14 +0900

    [test][jpashop] testcode for MemberRepository | 2022-03-31

--- jpashop/src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -1,4 +1,38 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
 public class MemberRepositoryTest {
-  
+
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+
+    @org.junit.Test
+    public void find() {
+    }
 }
