7e3b037bac6ea0216a05274604d5e8cc1f3baa4e
제유빈 <yubinje630@gmail.com>
2022-04-06 23:11:16 +0900

    [refactor][jpashop] refactor Member domain | 2022-04-06

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,17 +4,23 @@ package jpabook.jpashop.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
 public class Member {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="member_id") // column name 지정
     private Long id;
-    private String username;
+
+    private String name;
+
+    private Address address;
+
+    private List<Order> orders = new ArrayList<>();
 
 }
