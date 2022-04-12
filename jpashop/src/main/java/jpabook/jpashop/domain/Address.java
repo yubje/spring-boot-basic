122d924c2f8e19acce04daeea0a663e91dd2ca25
제유빈 <yubinje630@gmail.com>
2022-04-12 20:55:00 +0900

    [feat][jpashop] refactor Address domain | 2022-04-12

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,10 +6,22 @@ import lombok.Setter;
 import javax.persistence.Embeddable;
 
 @Embeddable
-@Getter @Setter
+@Getter
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    /* 생성할 때만 값을 만들고, 변경이 불가능하도록 개발 */
+
+    // default constructor(기본 생성자)
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
