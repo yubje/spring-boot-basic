c00600e4ce55237ec937a166f4ee0489ec5be327
제유빈 <yubinje630@gmail.com>
2022-04-14 23:38:43 +0900

    [feat][jpashop] add fetchtype lazy to entity | 2022-04-14

--- jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -8,6 +8,8 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Getter  @Setter
 public class Category {
@@ -25,7 +27,7 @@ public class Category {
     private List<Item> items = new ArrayList<>();
 
     // 동일 entity 내 self 연관관계 지정
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
