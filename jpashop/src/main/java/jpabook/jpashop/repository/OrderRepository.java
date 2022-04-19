69f44f542b6e485f9b281f4397b321f3f4912380
제유빈 <yubinje630@gmail.com>
2022-04-19 11:32:16 +0900

    [feat][jpashop] add Order Repository | 2022-04-19

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,2 +1,43 @@
-package jpabook.jpashop.repository;public class OrderRepository {
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+    private final EntityManager em;
+
+    /**
+     *  주문 저장
+     */
+    public void save(Order order) {
+        if (order.getId() == null) {
+            em.persist(order);
+        } else {
+            em.merge(order);
+        }
+    }
+
+    /**
+     *  주문 찾기
+     */
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+
+    /**
+     * 전체 주문 찾기
+     */
+    public List<Order> findAll() {
+        return em.createQuery("select * from Order o", Order.class)
+                .getResultList();
+    }
+
+
 }
