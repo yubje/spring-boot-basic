6d75b77e30b628f46b5813dd44b44b41a02d1793
제유빈 <yubinje630@gmail.com>
2022-04-20 22:14:58 +0900

    [test][jpashop] add Order Service Test | 2022-04-20

--- jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -66,11 +66,19 @@ public class OrderServiceTest {
     @Test
     public void cancelOrder() throws Exception {
         // given
+        Member member = createMember();
+        Book item = createBook("변신", 10000, 10);
 
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
 
         // when
+        orderService.cancelOrder(orderId);
 
         // then
+        Order getOrder = orderRepository.findOne(orderId);
+        assertEquals("주문 취소 시 상태는 CANCEL", OrderStatus.CANCEL, getOrder.getStatus());
+        assertEquals("주문이 취소된 상품은 그만큼 재고가 증가해야 한다.", 10, item.getStockQuantity()); 
     }
 
     /* 테스트 사용 목적 함수 */
