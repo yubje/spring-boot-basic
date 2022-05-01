7402cde3170e73b4e1f4340e59897cb02d52d50d
제유빈 <yubinje630@gmail.com>
2022-05-01 21:44:59 +0900

    [feat][jpashop] add update item functioanality on controller & add view settings  | 2022-05-01

--- jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -1,12 +1,16 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -33,11 +37,28 @@ public class ItemController {
         return "redirect:/";
     }
 
-    @GetMappingC("/items")
+    @GetMapping("/items")
     public String list(Model model) {
-        List<Item> items = itemService.findItems();
+        List<Item> items = itemService.finditems();
         model.addAttribute("items", items);
         return "items/itemList";
+    }
+
+    @GetMapping("items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book item = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+
     }
 
 }
