3160c997e64bc2b88de79b1e119768e6a5a5ba08
제유빈 <yubinje630@gmail.com>
2022-05-15 11:30:28 +0900

    [feat][jpabasic] add Member domain, add create logic | 2022-05-15

--- jpa-basic/src/main/java/jpabasic/JpaMain.java
+++ jpa-basic/src/main/java/jpabasic/JpaMain.java
@@ -1,11 +1,32 @@
 package jpabasic;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("name");
+
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
     }
 }
